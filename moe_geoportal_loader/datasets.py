116805427d7786ee39a46524fc6db443dd7c7212
Geo-Jagaimo <geo.jagaimo@gmail.com>
2025-10-05 22:38:42 +0900

    中大型哺乳類分布調査

--- moe_geoportal_loader/datasets.py
+++ moe_geoportal_loader/datasets.py
@@ -9,6 +9,21 @@ DATASETS = {
         "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/vgsk_{pref_code}/FeatureServer",
         "has_prefecture": True,
     },
+    "anaguma": {
+        "name": "中大型哺乳類分布調査(アナグマ)",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/anaguma/FeatureServer",
+        "has_prefecture": False,
+    },
+    "kitune": {
+        "name": "中大型哺乳類分布調査(キツネ)",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/kitune/FeatureServer",
+        "has_prefecture": False,
+    },
+    "tanuki": {
+        "name": "中大型哺乳類分布調査(タヌキ)",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/tanuki/FeatureServer",
+        "has_prefecture": False,
+    },
     "change_union_tokunoshima_5th": {
         "name": "変化_union_徳之島_第5回andR01Coralmap",
         "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/change_union_tokunoshima_5thandR01Coralmap/FeatureServer",

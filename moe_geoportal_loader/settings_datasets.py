011609be39f7cb6490d2c56fa6719174b033c00e
Geo-Jagaimo <geo.jagaimo@gmail.com>
2025-10-09 14:38:18 +0900

    add coral data

--- moe_geoportal_loader/settings_datasets.py
+++ moe_geoportal_loader/settings_datasets.py
@@ -24,6 +24,81 @@ DATASETS = {
         "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/tanuki/FeatureServer",
         "has_prefecture": False,
     },
+    "tokara_21_code1": {
+        "name": "サンゴ浅海生態系現況把握調査（トカラ列島周辺2021, CODE1）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/tokara_2021_code1/FeatureServer",
+        "has_prefecture": False,
+    },
+    "tokara_21_code2": {
+        "name": "サンゴ浅海生態系現況把握調査（トカラ列島周辺2021, CODE2）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/tokara_2021_code2/FeatureServer",
+        "has_prefecture": False,
+    },
+    "kume_18_code1": {
+        "name": "サンゴ浅海生態系現況把握調査（久米島2018, CODE1）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/kume_code1/FeatureServer",
+        "has_prefecture": False,
+    },
+    "kume_18_code2": {
+        "name": "サンゴ浅海生態系現況把握調査（久米島2018, CODE2）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/kume_code2/FeatureServer",
+        "has_prefecture": False,
+    },
+    "tarama_18_code1": {
+        "name": "サンゴ浅海生態系現況把握調査（多良間島2018, CODE1）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/tarama_code1/FeatureServer",
+        "has_prefecture": False,
+    },
+    "tarama_18_code2": {
+        "name": "サンゴ浅海生態系現況把握調査（多良間島2018, CODE2）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/tarama_code2/FeatureServer",
+        "has_prefecture": False,
+    },
+    "osumi_18_code1": {
+        "name": "サンゴ浅海生態系現況把握調査（大隅諸島周辺2021, CODE1）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/oosumi_2021_code1/FeatureServer",
+        "has_prefecture": False,
+    },
+    "osumi_18_code2": {
+        "name": "サンゴ浅海生態系現況把握調査（大隅諸島周辺2021, CODE2）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/oosumi_2021_code2/FeatureServer",
+        "has_prefecture": False,
+    },
+    "amami_1819_code1": {
+        "name": "サンゴ浅海生態系現況把握調査（奄美群島2018-2019, CODE1）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/amami_all_code1/FeatureServer",
+        "has_prefecture": False,
+    },
+    "amami_1819_code2": {
+        "name": "サンゴ浅海生態系現況把握調査（奄美群島2018-2019, CODE2）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/amami_all_code2/FeatureServer",
+        "has_prefecture": False,
+    },
+    "miyako_18_code1": {
+        "name": "サンゴ浅海生態系現況把握調査（宮古島2018, CODE1）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/miyako_code1/FeatureServer",
+        "has_prefecture": False,
+    },
+    "miyako_18_code2": {
+        "name": "サンゴ浅海生態系現況把握調査（宮古島2018, CODE2）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/miyako_code2/FeatureServer",
+        "has_prefecture": False,
+    },
+    "ogasawara_20_code1": {
+        "name": "サンゴ浅海生態系現況把握調査（小笠原周辺2020, CODE1）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/ogasawara_v3_code1/FeatureServer",
+        "has_prefecture": False,
+    },
+    "ogasawara_20_code2": {
+        "name": "サンゴ浅海生態系現況把握調査（小笠原周辺2020, CODE2）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/ogasawara_v3_code2/FeatureServer",
+        "has_prefecture": False,
+    },
+    "sekisei_17": {
+        "name": "サンゴ浅海生態系現況把握調査（石西礁湖2017）",
+        "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/sekiseishoko_2017/FeatureServer",
+        "has_prefecture": False,
+    },
     "tokunoshima_5th": {
         "name": "徳之島_第5回andR01Coralmap",
         "url": "https://svr-moej.gisservice.jp/arcgis/rest/services/Hosted/change_union_tokunoshima_5thandR01Coralmap/FeatureServer",

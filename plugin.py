535a407cd0287f57bfa07f1c7245b4fcc350b712
Geo-Jagaimo <geo.jagaimo@gmail.com>
2025-10-18 21:50:34 +0900

    delete icon in menu bar

--- plugin.py
+++ plugin.py
@@ -1,13 +1,7 @@
-import contextlib
-
 from qgis.core import QgsApplication
 from qgis.gui import QgisInterface
-from qgis.PyQt.QtWidgets import QAction, QToolButton
 
 from .data_loader.loader_provider import MOELoaderProvider
-
-with contextlib.suppress(ImportError):
-    from processing import execAlgorithmDialog
 
 
 class MOEGeoportalLoader:
@@ -18,33 +12,5 @@ class MOEGeoportalLoader:
         self.provider = MOELoaderProvider()
         QgsApplication.processingRegistry().addProvider(self.provider)
 
-        if self.iface:
-            self.setup_algorithm_tool_button()
-
     def unload(self):
-        self.teardown_algorithm_tool_button()
         QgsApplication.processingRegistry().removeProvider(self.provider)
-
-    def setup_algorithm_tool_button(self):
-        if hasattr(self, "toolButtonAction") and self.toolButtonAction:
-            return
-
-        tool_button = QToolButton()
-        icon = self.provider.icon()
-        default_action = QAction(
-            icon, self.tr("環境ジオポータルのデータを読み込む"), self.iface.mainWindow()
-        )
-        default_action.triggered.connect(
-            lambda: execAlgorithmDialog("moe:moe_geoportal_loader", {})
-        )
-        tool_button.setDefaultAction(default_action)
-
-        self.toolButtonAction = self.iface.addToolBarWidget(tool_button)
-
-    def teardown_algorithm_tool_button(self):
-        if hasattr(self, "toolButtonAction"):
-            self.iface.removeToolBarIcon(self.toolButtonAction)
-            del self.toolButtonAction
-
-    def tr(self, message):
-        return QgsApplication.translate("MOEGeoportalLoader", message)
